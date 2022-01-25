5d576970af60ec40b452f1dfbc858a33a489dec2
Pete Seeling <peteseeling@gmail.com>
2022-01-25 17:38:25 -0600

    back to original mapping for header/m/footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import Header from './Header.js';
-import Footer from './Footer.js';
-import Main from './Main.js';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Main from './Main/Main';
 import { animals } from './AnimalList';
 
 
