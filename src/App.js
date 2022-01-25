545e50d5df945d6771f635b299a84ba5a515f637
Pete Seeling <peteseeling@gmail.com>
2022-01-25 16:15:12 -0600

    fixed header.js route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Header from '.Header.js';
+import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js';
 import { animals } from './AnimalList';
