6a1d30b314fe88301062c117f9df7e0057500e04
Pete Seeling <peteseeling@gmail.com>
2022-01-25 12:44:27 -0600

    Header/footer/background img displaying.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import Footer from './Footer/Footer';
 import { animals } from './AnimalList';
 
 
 function App() {
   return (
     <div className="App">
-      <Header name= 'Pete' />
+      <Header name= 'Pete Seeling' />
       <Main animals={animals} />
+      <Footer year= '2022' />
     </div>
   );
 }
