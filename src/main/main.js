c4f60f5800d54ba5f924bfd1665a691052940712
Pete Seeling <peteseeling@gmail.com>
2022-01-24 19:56:02 -0600

    Header showing up. work on css for it

--- src/main/main.js
+++ src/main/main.js
@@ -1,3 +1,13 @@
-import React from 'react'
-import backgroundImg from '../background.png'
-import AnimalCard from ''
+import React from 'react';
+import backgroundImg from '../background.png';
+import AnimalCard from '../AnimalCard/AnimalCard';
+import './Main.css';
+
+export default function Main(props){
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+
+    </main>
+  );
+}
+
